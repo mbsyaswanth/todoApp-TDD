4a62ec60837c5d02a9c5c3ff88900533d78e1db9
M B S Yaswanth <mbs.yaswanth@gmail.com>
2019-08-02 14:50:43 +0530

    [red state] added test case for integration of input box and store

--- src/components/InputBox/InputBox.test.js
+++ src/components/InputBox/InputBox.test.js
@@ -1,14 +1,29 @@
 import TodoStore from "../../stores/TodoStore";
 import { filters } from "../../constants";
-import { render } from "@testing-library/react";
+import { render, fireEvent, cleanup } from "@testing-library/react";
 import React from "react";
 
 import InputBox from "./index";
+
+afterEach(cleanup);
 
 describe("InputBox component test cases ", () => {
   it("should check whether inputbox component exists ", () => {
     const { getByPlaceholderText } = render(<InputBox />);
     const result = getByPlaceholderText("what needs to be done ?");
     expect(result).toBeDefined();
+  });
+
+  it("should add entered todo to the store", () => {
+    const store = new TodoStore();
+    const spyfunc = jest.spyOn(store, "addTodo");
+    const { getByPlaceholderText } = render(
+      <InputBox submit={store.addTodo} />
+    );
+    const result = getByPlaceholderText("what needs to be done ?");
+    fireEvent.change(result, { target: { value: "a test entry" } });
+    fireEvent.keyDown(result, { key: "Enter", code: 13 });
+    expect(spyfunc).toBeCalledWith("a test entry");
+    expect(store.todos[0].description).toBe("a test entry");
   });
 });
