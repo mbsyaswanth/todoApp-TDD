da1973d3d6750be760210b7227fee8a94310c3b8
M B S Yaswanth <mbs.yaswanth@gmail.com>
2019-08-02 10:20:37 +0530

    [red state] added removeTodo test case

--- src/stores/TodoStore/TodoStore.test.js
+++ src/stores/TodoStore/TodoStore.test.js
@@ -1,10 +1,20 @@
 import TodoStore from "./index";
 
+let todoStore;
+beforeEach(() => {
+  todoStore = new TodoStore();
+});
+
 describe("TodoStore test cases", () => {
   it("should add todo item to todo list", () => {
-    const todoStore = new TodoStore();
     todoStore.addTodo("learn tdd");
     expect(todoStore.todos).toHaveLength(1);
     expect(todoStore.todos[0].description).toBe("learn tdd");
+  });
+
+  it("should remove the added todo from todos (removeTodo)", () => {
+    todoStore.addTodo("learn tdd");
+    todoStore.removeTodo();
+    expect(todoStore.todos).toHaveLength(0);
   });
 });
