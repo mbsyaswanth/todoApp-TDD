6ce27a6100fc737e23f1730e8c9ace9f2c928173
M B S Yaswanth <mbs.yaswanth@gmail.com>
2019-08-02 15:14:50 +0530

    [refactor state] refactored input box (added local state to track input change)

--- src/components/InputBox/index.js
+++ src/components/InputBox/index.js
@@ -1,16 +1,28 @@
 import React, { Component } from "react";
 
 class InputBox extends Component {
+  state = {
+    value: ""
+  };
   onSubmit = event => {
     if (event.keyCode === 13) {
-      this.props.submit(event.target.value);
+      this.props.submit(this.state.value);
     }
   };
+
+  updateState = event => {
+    this.setState({
+      value: event.target.value
+    });
+  };
+
   render() {
     return (
       <div>
         <input
           type="text"
+          value={this.state.value}
+          onChange={this.updateState}
           onKeyDown={this.onSubmit}
           placeholder="what needs to be done ?"
         />
