be08038448c768028b0d0c77dc23bc3f19bb67d4
M B S Yaswanth <mbs.yaswanth@gmail.com>
2019-08-03 13:24:49 +0530

    [red state] added test case for do not submit emplty entry

--- src/components/InputBox/InputBox.test.js
+++ src/components/InputBox/InputBox.test.js
@@ -27,4 +27,17 @@ describe("InputBox component test cases ", () => {
     expect(spyfunc).toBeCalledWith("a test entry");
     expect(store.todos[0].description).toBe("a test entry");
   });
+  it("should not sumbit empty input", () => {
+    const store = new TodoStore();
+    const spyfunc = jest.spyOn(store, "addTodo");
+    const { getByPlaceholderText } = render(
+      <InputBox submit={store.addTodo} />
+    );
+    const result = getByPlaceholderText("what needs to be done ?");
+    fireEvent.change(result, { target: { value: "" } });
+    fireEvent.keyDown(result, { key: "Enter", code: 13, keyCode: 13 });
+    expect(result.value).toBe("");
+    expect(spyfunc).not.toBeCalledWith("");
+    expect(store.todos.length).toBe(0);
+  });
 });
