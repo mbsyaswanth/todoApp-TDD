1e07dac95698e7b16caed2cccaf35a45567f8eb7
M B S Yaswanth <mbs.yaswanth@gmail.com>
2019-08-03 11:19:08 +0530

    [red state] added test case for todo footer

---
+++ src/components/TodoFooter/TodoFooter.test.js
@@ -0,0 +1,28 @@
+import TodoStore from "../../stores/TodoStore";
+import { filters } from "../../constants";
+import { render, fireEvent, cleanup } from "@testing-library/react";
+import React from "react";
+
+afterEach(cleanup);
+let todoStore;
+beforeEach(() => {
+  todoStore = new TodoStore();
+});
+
+describe("Todo footer", () => {
+  it("should set filter according according to the filter selected", () => {
+    jest.spyOn(todoStore, "setFilter");
+    const { getByDisplayValue } = render(
+      <TodoFooter setFilter={todoStore.setFilter} />
+    );
+    let result = getByDisplayValue("All");
+    fireEvent.click(result);
+    expect(todoStore.setFilter).toBeCalledWith(filters.all);
+    result = getByDisplayValue("Completed");
+    fireEvent.click(result);
+    expect(todoStore.setFilter).toBeCalledWith(filters.completed);
+    result = getByDisplayValue("Active");
+    fireEvent.click(result);
+    expect(todoStore.setFilter).toBeCalledWith(filters.active);
+  });
+});
